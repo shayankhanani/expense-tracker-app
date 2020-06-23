9fe4663e1293085716e7aab9d6397b7a9bacf486
Shayan Khanani <45587698+shayankhanani@users.noreply.github.com>
2020-06-23 23:33:35 +0500

    on Submit Input reset functionality added.

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -16,6 +16,7 @@ export const AddTransaction = () => {
             amount: +amount
         }
         addTransaction(newTransaction);
+        e.target.reset();
     }
     return (
         <div>
